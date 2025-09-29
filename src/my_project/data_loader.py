da88c8a282c273de9b80cc17c2890e0782d50fae
Margarita <kadochnikova.ritulya@mail.ru>
2025-09-29 20:35:03 +0300

    Fix file

--- src/my_project/data_loader.py
+++ src/my_project/data_loader.py
@@ -6,10 +6,13 @@ file_url = f"https://drive.google.com/uc?id={FILE_ID}"
 raw_data = pd.read_csv(file_url)
 print(raw_data.head(5))
 print(raw_data.dtypes)
-raw_data['education'] = raw_data['education'].astype('category')
+
+raw_data['education'] = raw_data['education'].round().astype('Int64').astype('category')
 raw_data['sex'] = raw_data['sex'].astype('category')
 raw_data['is_smoking'] = np.where(raw_data['is_smoking'] == 'YES', True, False)
+raw_data['BPMeds'] = raw_data['BPMeds'].astype('Int64')
 raw_data['TenYearCHD'] = np.where(raw_data['TenYearCHD'] == 1, True, False)
+
 print(raw_data.head(5))
 print(raw_data.dtypes)
 raw_data.to_csv('df.csv')
